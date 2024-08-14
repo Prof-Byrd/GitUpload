0389029b4ffc6a29a5c7b95b0943663df33094c1
Brian Byrd <brian.byrd@ecpi.edu>
2024-08-14 17:56:00 -0400

    1st Commit

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import './App.css';
+import Button from './Button';
+function App() {
+  const [count, setCount] = useState(0);
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+
+  const handleDecrement = () => {
+    setCount(count - 1);
+  };
+  const handleReset = () => {
+    setCount(0);
+  };
+  return (
+    <div className="App">
+      <h1>Counter: {count}</h1>
+      <Button label="Increment" onClick={handleIncrement} />
+      <Button label="Decrement" onClick={handleDecrement} />
+      <Button label="Reset" onClick={handleReset} />
+    </div>
+  );
+}
+export default App;
+/*
+import logo from './logo.svg';
+import './App.css';
+
+function Profile() {
+    return (
+      <img
+        src="https://i.imgur.com/MK3eW3As.jpg"
+        alt="Katherine Johnson"
+      />
+    );
+}
+export default function Gallery() {
+  return (
+  <section>
+    <h1>Amazing scientists</h1>
+    <Profile />
+    <Profile />
+    <Profile />
+  </section>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+// export default App;
+*/
